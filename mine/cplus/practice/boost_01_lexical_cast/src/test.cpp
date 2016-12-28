ed5d5dbc3325863e8a2b30ee161770e177139cc7
yangkai04 <yangck0410672@163.com>
2016-12-28 15:38:35 +0800

    add boost_01_lexical_cast

---
+++ mine/cplus/practice/boost_01_lexical_cast/src/test.cpp
@@ -0,0 +1,13 @@
+#include "test.h"
+
+#include <iostream>  
+
+#include <boost/lexical_cast.hpp>
+
+int main() {   
+	int a = boost::lexical_cast<int>("123"); 
+	double b = boost::lexical_cast<double>("123.12"); 
+	std::cout << a << std::endl; 
+	std::cout << b << std::endl; 
+        return 0; 
+}
