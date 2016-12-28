fb54125875190a91c6e4d5c46b02bfad92be5c6b
yangkai04 <yangck0410672@163.com>
2016-12-28 14:25:46 +0800

    add practice 01_rank_err

---
+++ mine/cplus/cmake/Options.cmake
@@ -0,0 +1,3 @@
+
+set(CMAKE_CXX_FLAGS "-g -O3 -pipe -Wextra -Wall -pthread -std=c++1y -fopenmp")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unused-parameter -Wno-deprecated -Wno-ignored-qualifiers -Wno-unused-local-typedefs -Wno-sign-compare -Wno-reorder")
