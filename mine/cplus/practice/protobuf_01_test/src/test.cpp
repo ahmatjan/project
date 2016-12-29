5b84cc5ee660d3985ad4dd6e219ccd52d3fafb32
yangkai04 <yangck0410672@163.com>
2016-12-29 21:47:30 +0800

    fix protobuf bug

--- mine/cplus/practice/protobuf_01_test/src/test.cpp
+++ mine/cplus/practice/protobuf_01_test/src/test.cpp
@@ -1,5 +1,7 @@
 #include "test.h"
 
+#include <iostream>
+
 #include "people.pb.h"
   
 #ifndef UNIT_TEST
@@ -7,6 +9,8 @@ int main(int argc, char *argv[]) {
 #else
 int inner_main(int argc, char *argv[]) {
 #endif
-    //std::cout << people.name() << std::endl;
+    demo::People people;
+    people.set_name("yangkai");
+    std::cout << people.name() << std::endl;
     return 0;  
 }
